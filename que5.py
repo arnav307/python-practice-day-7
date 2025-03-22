521cda969b0da04034863b78d692f0f1940cb43b
Arnavghimire <ghimirearnav738@gmail.com>
2025-03-22 23:08:57 +0545

    add it

---
+++ que5.py
@@ -0,0 +1,8 @@
+a={"Ram":8,
+   "shyam":18, 
+   "hari":30,
+   "kabindra": 24}
+print("The age of ram is ",a["Ram"])
+print("The age of shyam is ",a["shyam"])
+print("The age of hari is ",a["hari"])
+print("The age of kabindra is ",a["kabindra"])
